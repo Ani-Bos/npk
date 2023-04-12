bf7f3cd922b0a17024dae4471e48cb793df92efa
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2023-04-12 19:26:27 +0530

    Added few features

--- client/src/components/CropCard.js
+++ client/src/components/CropCard.js
@@ -2,29 +2,42 @@
 import test from '../static/rice.jpg'
 function CropCard() {
   return (
-    <div className=' my-7 px-5 md:px-0'>
-        <div className='font-bold text-xl pb-4'>Recommended Crop</div>
-<div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg  md:flex-row hover:bg-gray-100 dark:border-gray-200 dark:bg-gray-200 dark:hover:bg-gray-200  m-auto shadow-lg">
-   <div className='w-[100%] h-[100px] overflow-clip'> <img class="object-cover w-full rounded-t-lg h-[100%] md:h-96  md:w-48 md:rounded-none md:rounded-l-lg" src={test} alt=""/></div>
-    <div class="flex w-[100%] items-center gap-4 justify-between  px-3 leading-normal">
-      
-        <div class="mb-2 ">
-          <div className='text-2xl font-semibold tracking-tight text-gray-900 dark:text-gray-700 text-center'>Rice</div> 
-          <div className='text-gray-500 font-semibold'>
-            90%
+    <div className=" my-7 px-5 md:px-0">
+      <div className="font-bold text-xl pb-4">Recommended Crop</div>
+      <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg  md:flex-row hover:bg-gray-100 dark:border-gray-200 dark:bg-gray-200 dark:hover:bg-gray-200  m-auto shadow-lg">
+        <div className="w-[100%] h-[100px] overflow-clip">
+          {" "}
+          <img
+            class="object-cover w-full rounded-t-lg h-[100%] md:h-96  md:w-48 md:rounded-none md:rounded-l-lg"
+            src={test}
+            alt=""
+          />
+        </div>
+        <div class="flex w-[100%] items-center gap-4 justify-between  px-3 leading-normal">
+          <div class="mb-2 ">
+            <div className="text-2xl font-semibold tracking-tight text-gray-900 dark:text-gray-700 text-center">
+              Rice
+            </div>
+            <div className="text-gray-500 font-semibold">90%</div>
+          </div>
+
+          <div class="mb-3 font-normal text-green-300 dark:text-gray-400">
+            <div>
+              {/* <button className="px-3 py-2 bg-gray-700 rounded  text-white">
+                Change
+              </button> */}
+              <button
+                type="submit"
+                className="px-2 py-1 text-lg  text-white transition-colors duration-300 bg-green-500 rounded-md shadow hover:bg-green-600 focus:outline-none focus:ring-green-200 focus:ring-4"
+              >
+                Change
+              </button>
+            </div>
           </div>
         </div>
-
-        <div class="mb-3 font-normal text-green-300 dark:text-gray-400">
-          <div>
-            <button className='px-3 py-2 bg-gray-700 rounded  text-white'>Change</button>
-          </div>
-        </div>
+      </div>
     </div>
-</div>
-
-    </div>
-  )
+  );
 }
 
 export default CropCard
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -72,7 +72,7 @@ import AdbIcon from '@mui/icons-material/Adb';
 //               <li>
 //                 <a
 //                   href="/login"
-//                   class="inline-block rounded-3xl px-6 outline-1 py-1.5 text-base font-medium leading-7 text-gray-800 shadow-sm ring-1 ring-gray-600 hover:bg-green-300 hover:ring-green-500"
+//                   className="inline-block rounded-3xl px-6 outline-1 py-1.5 text-base font-medium leading-7 text-gray-800 shadow-sm ring-1 ring-gray-600 hover:bg-green-300 hover:ring-green-500"
 //                 >
 //                   LogIn
 //                 </a>
@@ -241,41 +241,61 @@ import AdbIcon from '@mui/icons-material/Adb';
 function Navbar() {
   return (
     <div>
-      
-<nav class="bg-white dark:bg-white  w-full z-20 top-0 left-0 border-b border-gray-200 ">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center">
-      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo"/>
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-  </a>
-  <div class="flex md:order-2">
-      <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Login</button>
-      {/* <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-g-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+      <nav className="bg-[#fffa92] dark:bg-[#fffa92]  w-full z-20 top-0 left-0 border-b border-transparent ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex md:order-2">
+            <a href='/login'
+            
+              className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              Login
+            </a>
+
+            {/* <button
+              type="button"
+              className="inline-block rounded-3xl px-6 outline-1 py-1.5 text-base font-medium leading-7 text-gray-800 shadow-sm ring-1 ring-gray-600 hover:bg-transparent hover:outline rounded-full"
+            >
+              Login
+            </button> */}
+
+            {/* <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-g-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button> */}
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    {/* <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white dark:border-white">
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            {/* <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white dark:border-white">
       <li>
-        <a href="/" class="block py-2 pl-3 pr-4 text-blue bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+        <a href="/" className="block py-2 pl-3 pr-4 text-blue bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
       </li>
       <li>
-        <a href="/about" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        <a href="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
       </li>
       <li>
-        <a href="/services" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        <a href="/services" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
       </li>
       <li>
-        <a href="/contact" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="/contact" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
       </li>
     </ul> */}
-  </div>
-  </div>
-</nav>
-
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar
