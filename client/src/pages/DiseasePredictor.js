5a9f8ce38ee7fcd5ca2c54f2c3d8ad3b634ff72b
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2023-04-14 16:34:36 +0530

    ui fixes

--- client/src/pages/DiseasePredictor.js
+++ client/src/pages/DiseasePredictor.js
@@ -58,28 +58,42 @@ navigate('/disease_predictor_result')
 //    );
         }
   return (
-    <div className='container m-auto px-5'>
-      <img className='hidden' alt="yoyo" id='temp' />
-        <div className='text-center font-bold my-5 text-xl'>Upload Image</div>
-<div class="grid grid-cols-[40%_60%] h-[20vh] overflow-hidden bg-white border border-gray-200 rounded-lg shadow flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-  <div className='border border-r-[1px] flex flex-col' style={{backgroundImage:`url(${img})`,backgroundSize:"cover"}}>
-  {/* <img class=" rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={img} alt=""/> */}
-    </div>  
-    <div class="flex flex-col justify-between p-4 leading-normal">
-       <div className='flex flex-col gap-4'>
-       <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"  onChange={handlechange}/>
-       <div>
-        <button onClick={handlepredict} className='px-1 py-1 bg-slate-400 rounded text-sm'>Predict disease</button>
-       </div>
-       </div>
+    <div className="container m-auto px-5">
+      <img className="hidden" alt="yoyo" id="temp" />
+      <div className="text-center font-bold my-5 text-xl">
+        Upload <span className="  text-green-500 text-center font-bold my-5 text-xl">Image</span>
+      </div>
+      <div class="grid grid-cols-[40%_60%] h-[20vh] overflow-hidden bg-white border border-gray-200 rounded-lg shadow flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div
+          className="border border-r-[1px] flex flex-col"
+          style={{ backgroundImage: `url(${img})`, backgroundSize: "cover" }}
+        >
+          {/* <img class=" rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={img} alt=""/> */}
+        </div>
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <div className="flex flex-col gap-4">
+            <input
+              class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              id="file_input"
+              type="file"
+              onChange={handlechange}
+            />
+            <div>
+              <button
+                onClick={handlepredict}
+                className="px-1 py-1 bg-green-400 rounded text-sm "
+              >
+                Predict disease
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-6">
+        <ProgressBar progressPercentage={load} />
+      </div>
     </div>
-</div>
-<div className='my-6'>
-<ProgressBar progressPercentage={load}/>
-</div>
-  
-    </div>
-  )
+  );
 }
 
 export default DiseasePredictor
