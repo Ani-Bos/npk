98004064e24b1c213a5d5287ba4c8afe7241d2ec
sachin <sksachin7z2@gmail.com>
2023-04-18 20:22:39 +0530

    image renamed

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 
-
+const dotenv=require('dotenv')
+dotenv.config()
 // const mongoURI="mongodb://localhost:27017/npk?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 const mongoURI=`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.qkqwkdq.mongodb.net/npk?retryWrites=true&w=majority`
 const connectToMongo=()=>{
