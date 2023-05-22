00fd9d706944c568d37205bb3c4498567930d753
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2023-05-22 20:26:03 +0530

    Frontend fixation

--- client/src/components/DroughtPred.js
+++ client/src/components/DroughtPred.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import test from '../static/Rice.jpg'
+// import test from '../static/bb.jpg'
 function DroughtPred() {
   return (
     <div>
           <div className='my-7 md:px-0'>
   <div class="grid rounded-lg">
   <div class="grid-cols-6">
-    <div style={{backgroundImage:`url(${test})`,backgroundSize:"cover"}} className="object-cover w-full rounded-lg h-[20vh] md:w-auto md:h-auto md:rounded-none md:rounded-l-lg">
+    <div style={{backgroundImage:`url('Images/bb.jpg')`,backgroundSize:"cover"}} className="object-cover w-full rounded-lg h-[20vh] md:w-auto md:h-auto md:rounded-none md:rounded-l-lg">
       <div class="card-overlay rounded-lg h-[20vh]">
         <div class="white-text text-center">
           <div class="card flex items-center">
